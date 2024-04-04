1b8e3f3956ebb94ace5f650854970c34705af908
Mekki Harboub <hrbmakki@gmail.com>
2024-04-04 07:00:18 +0100

    Update apiConfig.js

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,8 +1,6 @@
-const apiKey = 'bb9f74affb49b3d92574298dccf8e0c4'; 
-
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: apiKey,
+    apiKey: 'bb9f74affb49b3d92574298dccf8e0c4',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
