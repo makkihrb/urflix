c617218b04bf32975ff8aa4c4946e5e8b1caf564
Mekki Harboub <hrbmakki@gmail.com>
2024-04-04 06:56:05 +0100

    Update apiConfig.js
    const apiKey

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,8 @@
+const apiKey = process.env.API_KEY;
+
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'process.env.API_KEY', // <= YOUR TMDB API KEY HERE
+    apiKey: apiKey,
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
