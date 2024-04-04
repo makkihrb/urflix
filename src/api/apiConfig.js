a0c05524e9c39d330611279829fda718773f5773
Mekki Harboub <hrbmakki@gmail.com>
2024-04-04 06:53:45 +0100

    Update apiConfig.js
    Create an env.local with your TMDB API

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: 'YOUR_API_KEY', // <= YOUR TMDB API KEY HERE
+    apiKey: 'process.env.API_KEY', // <= YOUR TMDB API KEY HERE
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
